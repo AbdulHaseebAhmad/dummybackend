8a6185966e773cfbdf4916beaaf22e37158396a1
Abdul Haseeb Ahmad Bashir <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-25 00:18:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 const app = express();
 app.get('/api/test', (request, response)=> {
-    response.send("App is Running Smooth ")
+    response.json("App is Running Smooth ")
 })
 app.use(express.json());
 
