b6be5e258af1e125138deb7aa051b4beec70937a
Abdul Haseeb Ahmad Bashir <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-25 00:32:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.listen(5001,()=> {
+    console.log(`App is Running Smooth on ${5001}`)
+})
 
 app.get('/api/test', (request, response)=> {
     response.send("App is Running Smooth ")
 })
 
-app.listen(5001,()=> {
-    console.log(`App is Running Smooth on ${5001}`)
-})
