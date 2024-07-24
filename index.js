f63ed0d57760b2f09084c41909f3d0ac50c4ce33
Abdul Haseeb Ahmad Bashir <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-25 00:31:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
-import dotenv from "dotenv"
-dotenv.config()
+
 const app = express();
-app.get('/api/test', (request, response)=> {
-    response.json("App is Running Smooth ")
-})
+
 app.use(express.json());
 
-const PORT = process.env.PORT
 
-
+app.get('/api/test', (request, response)=> {
+    response.send("App is Running Smooth ")
+})
 
 app.listen(5001,()=> {
     console.log(`App is Running Smooth on ${5001}`)
